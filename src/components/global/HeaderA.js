f94cca562d69e983a3f092623f44619f8787617c
hanhanxue <hanhanxue.hhx@gmail.com>
2023-08-29 12:53:48 -0400

    no scroll issue solved. need to not have min height but just height for the container

--- src/components/global/HeaderA.js
+++ src/components/global/HeaderA.js
@@ -73,82 +73,84 @@ export default function HeaderA() {
             ${styles.navModal}
             ${isMenuOpen ? styles.navModalOpen : ''}
         `}>
-            <header className={`${styles.headerGrid} `}>
-                <div className={`${styles.tile} ${styles.logo}`} style={{transitionDelay: `${delayDurations[0]}ms`}}>   
-                    <Link href="/"><p className={`brand-base ${styles.logotype}`}>
-                        Super Manifold®
-                    </p></Link>
-                </div>
-                <div className={`${styles.tile} ${styles.menu} `} style={{transitionDelay: `${delayDurations[1]}ms`}}>
-                    <Burger isOpen={isMenuOpen} onMenuClick={handleMenuClick}/>
-                </div >
-            </header>
+                {/* 01A Default Header */}
+                <header className={`${styles.headerGrid} `}>
+                    <div className={`${styles.tile} ${styles.logo}`} style={{transitionDelay: `${delayDurations[0]}ms`}}>   
+                        <Link href="/"><p className={`brand-base ${styles.logotype}`}>
+                            Super Manifold®
+                        </p></Link>
+                    </div>
+                    <div className={`${styles.tile} ${styles.menu} `} style={{transitionDelay: `${delayDurations[1]}ms`}}>
+                        <Burger isOpen={isMenuOpen} onMenuClick={handleMenuClick}/>
+                    </div >
+                </header>
+                {/* 01B Small Screen Header */}
+                <header className={`${styles.headerGridSmall} ${styles.tile} `} style={{transitionDelay: `${delayDurations[8]}ms`}}>
+                    <div className={`${styles.logo}`}>   
+                        <Link href="/"><p className={`brand-base ${styles.logotype}`}>
+                            Super Manifold®
+                        </p></Link>
+                    </div>
+                    <div className={`${styles.menu} `}>
+                        <Burger isOpen={isMenuOpen} onMenuClick={handleMenuClick}/>
+                    </div >
+                </header>
+                {/* 02 Nav Section */}
+                <nav className={`${styles.navGrid}`}>
+                    <div className={`${styles.tile} ${styles.tile_2c_1r}`} style={{transitionDelay: `${delayDurations[2]}ms`}}>
+                        <h2 className={`brand-xl ${styles.tileLabel} `}>Work</h2>
+                    </div> 
 
-            <header className={`${styles.headerGridSmall} ${styles.tile} `} style={{transitionDelay: `${delayDurations[8]}ms`}}>
-                <div className={`${styles.logo}`}>   
-                    <Link href="/"><p className={`brand-base ${styles.logotype}`}>
-                        Super Manifold®
-                    </p></Link>
-                </div>
-                <div className={`${styles.menu} `}>
-                    <Burger isOpen={isMenuOpen} onMenuClick={handleMenuClick}/>
-                </div >
-            </header>
+                    <div className={`${styles.tile} ${styles.tile_2c_1r}`} style={{transitionDelay: `${delayDurations[5]}ms`}}>
+                        <h2 className={`brand-xl ${styles.tileLabel} `}>Process</h2>
+                    </div>
 
+                    <div className={`${styles.tile} ${styles.tile_1c_1r}`} style={{transitionDelay: `${delayDurations[3]}ms`}}>  
+                        <h2 className={`brand-xl ${styles.tileLabel} `}>Studio</h2>
+                    </div>
 
-            <nav className={`${styles.navGrid}`}>
-                <div className={`${styles.tile} ${styles.tile_2c_1r}`} style={{transitionDelay: `${delayDurations[2]}ms`}}>
-                    <h2 className={`brand-xl ${styles.tileLabel} `}>Work</h2>
-                </div> 
+                    <div className={`${styles.tile} ${styles.tile_1c_1r}`} style={{transitionDelay: `${delayDurations[6]}ms`}}>
+                        <h2 className={`brand-xl ${styles.tileLabel} `}>{`Let's chat`}</h2>
+                    </div>
 
-                <div className={`${styles.tile} ${styles.tile_2c_1r}`} style={{transitionDelay: `${delayDurations[5]}ms`}}>
-                    <h2 className={`brand-xl ${styles.tileLabel} `}>Process</h2>
-                </div>
-
-                <div className={`${styles.tile} ${styles.tile_1c_1r}`} style={{transitionDelay: `${delayDurations[3]}ms`}}>  
-                    <h2 className={`brand-xl ${styles.tileLabel} `}>Studio</h2>
-                </div>
-
-                <div className={`${styles.tile} ${styles.tile_1c_1r}`} style={{transitionDelay: `${delayDurations[6]}ms`}}>
-                    <h2 className={`brand-xl ${styles.tileLabel} `}>{`Let's chat`}</h2>
-                </div>
-
-                <div className={`${styles.tile} ${styles.tile_1c_2r}`} style={{transitionDelay: `${delayDurations[4]}ms`}}>
-                    <h2 className={`brand-xl ${styles.tileLabel} `}>Contact</h2>
-                    <div className={`${styles.tileCopy} `}>
+                    <div className={`${styles.tile} ${styles.tile_1c_2r}`} style={{transitionDelay: `${delayDurations[4]}ms`}}>
+                        <h2 className={`brand-xl ${styles.tileLabel} `}>Contact</h2>
+                        <div className={`${styles.tileCopy} `}>
+                            <ul className={`brand-base `}>
+                                <li>
+                                    hello@supermnf<br/>
+                                    +1 628 523 5241
+                                </li>
+                                <li>
+                                    Super Manifold Inc.<br/>
+                                    5335 Saint-Laurent<br/>
+                                    Montreal QC H2T 1S1<br/>
+                                    Canada
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                {/* 03 Copyright Section */}
+                <div className={`${styles.footerGrid} ${styles.tile}`} style={{transitionDelay: `${delayDurations[7]}ms`}}>
+                    <div className={`${styles.copyright}`}>
+                        <p className={`brand-base`}>©2023</p>
+                    </div>
+                    <div className={`${styles.social}`}>
                         <ul className={`brand-base `}>
-                            <li>
-                                hello@supermnf<br/>
-                                +1 628 523 5241
-                            </li>
-                            <li>
-                                Super Manifold Inc.<br/>
-                                5335 Saint-Laurent<br/>
-                                Montreal QC H2T 1S1<br/>
-                                Canada
-                            </li>
+                            <li>Github</li>
+                            <li>Instagram</li>
+                            <li>LinkedIn</li>
                         </ul>
                     </div>
                 </div>
-            </nav>
+            {/* <div className={`${styles.scrollable}`}>
 
-
-
-
-            <div className={`${styles.footerGrid} ${styles.tile}`} style={{transitionDelay: `${delayDurations[7]}ms`}}>
-                <div className={`${styles.copyright}`}>
-                    <p className={`brand-base`}>©2023</p>
-                </div>
-                <div className={`${styles.social}`}>
-                    <ul className={`brand-base `}>
-                        <li>Github</li>
-                        <li>Instagram</li>
-                        <li>LinkedIn</li>
-                    </ul>
-                </div>
-            </div>
+            </div> */}
 
         </div>
+
+  
   
         </>
     )
